d96c5df380a3d5a7ad96d6c59889be45297e16cf
Royce <58853882+Roycem5x@users.noreply.github.com>
2019-12-18 11:51:49 -0500

    took out the commands for read me file

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ module.exports = {
 }
 // put inside a folder called "commands" and make it a js file, name the file "ping"
 
-
-
-
-// How to upload the file to github
-// 1) Make the changes
-// 2) Save the changes
-// 3) open the folder through right click and select open with git bash
-// 4) Or just cd to the directory
-// 5) Put down the code
-// - git add .
-// - git commit -m "What you changeed"
-// - git push origin master
-// This sends the file to github, now we need to redoply the file through heroku
-// Go to https://dashboard.heroku.com/apps/coalitionofdevils/deploy/github
-// Scroll down to manual deploy
-// click deploy branch
-// The procfile makes it so the bot stays up 24/7
-
 // variables
 const Discord = require('discord.js');
 const bot = new Discord.Client();
